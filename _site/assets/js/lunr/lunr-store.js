60592fc3629f3a1747d29a7ceacacec0897acaec
unknown <adm.jalfaro@australiansuper.com>
2020-08-06 01:24:57 +1000

    Added first Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Terraform API using PowerShell - Workspace Information",
+        "excerpt":"🚀Hello there, This is my first post and I hope it helps someone and remember constructive feedback is always welcome🚀. I have been working on getting on getting Workspace information in Terraform Enterprise/Cloud. I manage a large number of workspaces and Terraform Cloud/ Enterprise does not show the Workspace Version...","categories": ["Blog"],
+        "tags": ["Terraform Cloud","Terraform Enterprise","Powershell"],
+        "url": "http://localhost:4000/blog/TFC/",
+        "teaser": "http://localhost:4000/"
+      }]
