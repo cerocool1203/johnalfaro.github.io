54579671d0b4f22d263d1924a1afcd556c99fa4d
unknown <adm.jalfaro@australiansuper.com>
2020-08-27 00:05:31 +1000

    for_each article Terraform

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Terraform API using PowerShell - Workspace Information",
-        "excerpt":"🚀Hello there, This is my first post and I hope it helps someone and remember constructive feedback is always welcome🚀. I have been working on getting on getting Workspace information in Terraform Enterprise/Cloud. I manage a large number of workspaces and Terraform Cloud/ Enterprise does not show the Workspace Version...","categories": ["Blog"],
+        "excerpt":"🚀Hello there, This is my first post and I hope it helps someone and remember constructive feedback is always welcome🚀. I have been working on getting on getting Workspace information in Terraform Enterprise/Cloud. I manage a large number of workspaces and Terraform Cloud/ Enterprise does not show the terraform version...","categories": ["Blog"],
         "tags": ["Terraform Cloud","Terraform Enterprise","Powershell"],
         "url": "http://localhost:4000/blog/TFC/",
+        "teaser": "http://localhost:4000/"
+      },{
+        "title": "Terraform 13 - for_each - Test Drive",
+        "excerpt":"🚀Terraform 0.13 is the new trend in the Terraform world IaC and one of the new features is the most wanted for_each🚀. I have all my Azure infrastructure currently deployed using Terraform 12. Given we already got v13 which judging for the number indicates it maybe unlucky, but 2020 cannot...","categories": ["Blog"],
+        "tags": ["Terraform Cloud","Terraform Enterprise","Terraform 13","Azure"],
+        "url": "http://localhost:4000/blog/T13-for_each/",
         "teaser": "http://localhost:4000/"
       }]
