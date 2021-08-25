b92f1892fc7ee1df88ac7173b734cee23ac8dc25
unknown <adm.jalfaro@australiansuper.com>
2021-08-25 23:03:05 +1000

    updated MM version 4.24

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,30 +2,30 @@ var store = [{
         "title": "Terraform API using PowerShell - Workspace Information",
         "excerpt":"🚀Hello there, This is my first post and I hope it helps someone and remember constructive feedback is always welcome🚀. I have been working on getting on getting Workspace information in Terraform Enterprise/Cloud. I manage a large number of workspaces and Terraform Cloud/ Enterprise does not show the terraform version...","categories": ["Blog"],
         "tags": ["Terraform Cloud","Terraform Enterprise","Powershell"],
-        "url": "http://localhost:4000/blog/TFC/",
-        "teaser": "http://localhost:4000/"
+        "url": "/blog/TFC/",
+        "teaser": ""
       },{
         "title": "Terraform 13 - for_each - Test Drive",
         "excerpt":"🚀Terraform 0.13 is the new trend in the Terraform world IaC and one of the new features is the most wanted for_each🚀. I have all my Azure infrastructure currently deployed using Terraform 12. Given we already got v13 which judging for the number indicates it maybe unlucky, but 2020 cannot...","categories": ["Blog"],
         "tags": ["Terraform Cloud","Terraform Enterprise","Terraform 13","Azure"],
-        "url": "http://localhost:4000/blog/T13-for_each/",
-        "teaser": "http://localhost:4000/"
+        "url": "/blog/T13-for_each/",
+        "teaser": ""
       },{
         "title": "Terraform Cloud - Test Drive Series",
         "excerpt":"I have been deploying my infrastructure using Terraform Open Source. However, given you can use Terraform Cloud for free you get some features like create and provision infrastructure as well as collaboration which you can do it up to 5 people… which is perfect for what I wanted to achieve....","categories": ["Blog"],
         "tags": ["Terraform Cloud","Terraform Enterprise","Azure"],
-        "url": "http://localhost:4000/blog/TerraformCloud/",
-        "teaser": "http://localhost:4000/"
+        "url": "/blog/TerraformCloud/",
+        "teaser": ""
       },{
         "title": "Deploying Private AKS using Bicep, because ARM is not strong enough :🦾!!!",
         "excerpt":"Project Bicep is taking ARM to the next level and it is making lots of good noise in the IaC world. I have been using terraform for a while so I decided to check it out and see how it compares to HCL language to deploy IaC in Azure Platform...","categories": ["Blog"],
         "tags": ["Project Bicep","Azure CLI","Azure","Terraform"],
-        "url": "http://localhost:4000/blog/Bicep/",
-        "teaser": "http://localhost:4000/"
+        "url": "/blog/Bicep/",
+        "teaser": ""
       },{
         "title": "A continuous Deployment Recipe consuming Azure DevOps 👉 PowerShell 👉 Terraform Cloud(API-driven Run Workflow)",
         "excerpt":"I have been working with Terraform Cloud/Enterprise using the Version Control System VCS which is good but to my opinion does not provide enough flexibility when you want to have a proper Continuous Deployment flow where you are not just deploying Infrastructure, but you are covering the cycle from Continuous...","categories": ["Blog"],
         "tags": ["Azure Web App","PowerShell","Azure DevOps","Terraform Cloud"],
-        "url": "http://localhost:4000/blog/CICD/",
-        "teaser": "http://localhost:4000/"
+        "url": "/blog/CICD/",
+        "teaser": ""
       }]
